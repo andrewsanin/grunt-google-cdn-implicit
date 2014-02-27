fa4c7d1c33d7bd97b59d802eeb24a4aa73933dfa
Pavel Nezhencev <pavel.nezhencev@creuna.com>
2014-02-27 18:59:43 +0200

    Implicit scripts injection from bower.json, replaced google-cdn lib with local data without versions

--- tasks/cdnify.js
+++ tasks/cdnify.js
@@ -1,25 +1,40 @@
 'use strict';
 
 var path = require('path');
-var googlecdn = require('google-cdn');
-var bowerConfig = require('bower').config;
 
+var googleCdnPrefix = '//ajax.googleapis.com/ajax/libs/';
+var cdnMap = {
+  'angular-latest': 'angularjs/{ver}/angular.min.js',
+  'dojo': 'dojo/{ver}/dojo/dojo.js',
+  'jquery': 'jquery/{ver}/jquery.min.js',
+  'jquery-mobile': 'jquerymobile/{ver}/jquery.mobile.min.js',
+  'jquery.ui': 'jqueryui/{ver}/jquery-ui.min.js',
+  'mootools': 'mootools/{ver}/mootools-yui-compressed.js',
+  'swfobject': 'swfobject/{ver}/swfobject.js',
+  'webfontloader': 'webfont/{ver}/webfont.js'
+}
 
 module.exports = function (grunt) {
 
-  grunt.registerMultiTask('cdnify', 'replace scripts with refs to the Google CDN', function () {
+  grunt.registerMultiTask('cdnify', 'add specified Google CDN script refs to html', function () {
+    // get specific js files
+    var jsToCdn = this.data.components;
     // collect files
     var files = grunt.file.expand({ filter: 'isFile' }, this.data.html);
-    var compJson = grunt.file.readJSON(bowerConfig.json);
+    var compJson = grunt.file.readJSON(this.data.bowerConfig);
+    var depsToCdn = [];
 
-    // Strip the leading path segment off, e.g. `app/bower_components` ->
-    // `bower_components`
-    var bowerDirBits = bowerConfig.directory.split(path.sep);
-    bowerDirBits.shift();
-    var componentsPath = bowerDirBits.join(path.sep);
+    for (var dependency in compJson.dependencies) {
+      if (jsToCdn.indexOf(dependency) != -1) {
+        depsToCdn.push({name: dependency, version: compJson.dependencies[dependency]});
+      }
+    }
 
-    grunt.log
-      .writeln('Going through ' + grunt.log.wordlist(files) + ' to update script refs');
+    if (depsToCdn.length == 0) {
+      return;
+    }
+
+    grunt.log.writeln('Going through ' + grunt.log.wordlist(files) + ' to update script refs');
 
     files = files.map(function (filepath) {
       return {
@@ -30,8 +45,20 @@ module.exports = function (grunt) {
 
     files.forEach(function (file) {
       var content = file.body;
+      var scriptsRefStr = '';
 
-      content = googlecdn(content, compJson, { componentsPath: componentsPath });
+      depsToCdn.forEach(function (dep) {
+        if (!(dep.name in cdnMap)) {
+          grunt.fail.warn('"' + dep.name + '" is unknown bower component. I know only ' + Object.keys(cdnMap));
+          return;
+        }
+
+        var ver = dep.version.replace('~', '').replace('>=', '').replace('^', '').replace('x', '0');
+        var cdnUrl = googleCdnPrefix + cdnMap[dep.name].replace('{ver}', ver);
+        scriptsRefStr += '    <script src="' + cdnUrl + '"></script>\r\n';
+      });
+      
+      content = content.replace('    <!-- build:cdn -->\r\n', scriptsRefStr)
       grunt.file.write(file.path, content);
     });
   });
