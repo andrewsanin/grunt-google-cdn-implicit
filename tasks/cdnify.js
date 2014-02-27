3ffbdc788c2843ed419e08ed0be42aa1f4bb31c4
Pavel Nezhencev <pavel.nezhencev@creuna.com>
2014-02-27 22:01:09 +0200

    Local fallbacks and almond support

--- tasks/cdnify.js
+++ tasks/cdnify.js
@@ -17,16 +17,36 @@ var cdnMap = {
 module.exports = function (grunt) {
 
   grunt.registerMultiTask('cdnify', 'add specified Google CDN script refs to html', function () {
-    // get specific js files
     var jsToCdn = this.data.components;
+    var almond = this.data.almond;
+    var options = this.data.options || {};
+    options = {
+      localFallback: options.localFallback,
+      bowerJson: options.bowerJson || 'bower.json',
+      bowerDir: options.bowerDir || 'bower_components'
+    };
+
     // collect files
     var files = grunt.file.expand({ filter: 'isFile' }, this.data.html);
-    var compJson = grunt.file.readJSON(this.data.bowerConfig);
+    var compJson = grunt.file.readJSON(options.bowerJson);
     var depsToCdn = [];
 
     for (var dependency in compJson.dependencies) {
-      if (jsToCdn.indexOf(dependency) != -1) {
-        depsToCdn.push({name: dependency, version: compJson.dependencies[dependency]});
+      if (dependency in jsToCdn) {
+        var localPath = jsToCdn[dependency].localPath;
+
+        if (options.localFallback && !localPath) {
+          var depDir = options.bowerDir + '/' + dependency;
+          var depBowerJson = grunt.file.readJSON(depDir + '/bower.json');
+          localPath = depDir + '/' + depBowerJson.main;
+        }
+        
+        depsToCdn.push({
+          name: dependency,
+          version: compJson.dependencies[dependency],
+          localPath: localPath,
+          success: jsToCdn[dependency].success
+        });
       }
     }
 
@@ -47,6 +67,11 @@ module.exports = function (grunt) {
       var content = file.body;
       var scriptsRefStr = '';
 
+      // almond support
+      if (almond && compJson.dependencies['almond']) {
+        scriptsRefStr += '    <script src="/' + almond + '"></script>\r\n';
+      }
+
       depsToCdn.forEach(function (dep) {
         if (!(dep.name in cdnMap)) {
           grunt.fail.warn('"' + dep.name + '" is unknown bower component. I know only ' + Object.keys(cdnMap));
@@ -56,9 +81,19 @@ module.exports = function (grunt) {
         var ver = dep.version.replace('~', '').replace('>=', '').replace('^', '').replace('x', '0');
         var cdnUrl = googleCdnPrefix + cdnMap[dep.name].replace('{ver}', ver);
         scriptsRefStr += '    <script src="' + cdnUrl + '"></script>\r\n';
+
+        if (options.localFallback) {
+          if (!dep.success) {
+            dep.success = 'window.' + dep.name;
+            grunt.log.writeln('CDN load success expression for "' + dep.name + '" component is not specified. Using "' + dep.success + '" expression.');
+          }
+
+          scriptsRefStr += '    <script>(' + dep.success + ')||document.write(\'<script src="/' + dep.localPath + '"><\\/script>\')</script>\r\n';
+        }
       });
       
-      content = content.replace('    <!-- build:cdn -->\r\n', scriptsRefStr)
+      content = content.replace('    <!-- build:cdn -->\r\n', scriptsRefStr);
+
       grunt.file.write(file.path, content);
     });
   });
